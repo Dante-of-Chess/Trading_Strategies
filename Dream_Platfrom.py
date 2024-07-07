5821688220a006908fef071936549bdd830eab81
Dante <adnashka@gmail.com>
2024-07-07 22:40:46 +0200

    Portfolio_Tracking

---
+++ Dream_Platfrom.py
@@ -0,0 +1,9 @@
+import pandas 
+
+
+class Portfolio:
+    def __init__(self,stock,share_size,cost_amount):
+        self.stock=stock
+        self.share_size=share_size
+        self.cost_amount=cost_amount
+        
